33ed0ca18b4eaaacdb8d6116e833a234a5a2bf2a
xuvigan <66873030+xuviga@users.noreply.github.com>
2025-04-13 13:50:36 +0300

    Fix

--- src/main/java/zmaster587/advancedRocketry/tile/hatch/TileInvHatch.java
+++ src/main/java/zmaster587/advancedRocketry/tile/hatch/TileInvHatch.java
@@ -4,6 +4,11 @@ import zmaster587.libVulpes.tile.multiblock.hatch.TileInventoryHatch;
 
 public class TileInvHatch extends TileInventoryHatch {
 
+    // 👇 Добавляем обязательный конструктор без аргументов
+    public TileInvHatch() {
+        this(1); // ← можно изменить размер слотов на нужный
+    }
+
     public TileInvHatch(int invSize) {
         super(invSize);
     }
